77f998f9defca06cd58cd571cbb95dfb1c534d5f
punya <punya8144@gmail.com>
2026-01-14 13:58:21 +0530

    up:env

--- dashboard-frontend/src/services/api.js
+++ dashboard-frontend/src/services/api.js
@@ -1,21 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Add a request interceptor to add the auth token to every request
 api.interceptors.request.use(
   (config) => {
-    const userInfo = localStorage.getItem('userInfo')
-      ? JSON.parse(localStorage.getItem('userInfo'))
-      : null;
-    
+    const userInfo = localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null;
+
     if (userInfo && userInfo.token) {
       config.headers.Authorization = `Bearer ${userInfo.token}`;
     }
